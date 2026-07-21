61b970acc6fef758cf7dc4ead7a3f89a7dedccb9
Paul Murray <pmurray@bigpond.com>
2026-07-21 14:53:32 +1000

    test commit

---
+++ commit.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 commit message"
+    exit 1
+fi
+
+MESSAGE="$*"
+
+git add web/
+git commit -m "$MESSAGE"
+TAG=$(git describe --tags --always)
+git log -1 --format='{"commit":"%h","hash":"%H","timestamp":"%cI","message":"%s","tag"="'$TAG'","ref":"%D"}' > web/js/commit.json
+git log -1 --oneline
+git add web/js/commit.json
+git commit -m "$MESSAGE"
