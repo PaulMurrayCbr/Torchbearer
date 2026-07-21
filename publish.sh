03fdccd5e1aa1215aeac845b5186d239a77a00ff
Paul Murray <pmurray@bigpond.com>
2026-07-21 20:06:23 +1000

    publish

---
+++ publish.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cp ~/git/Torchbearer/web/* ~/git/PaulMurrayCbr.github.io/Torchbearer
+pushd ~git/PaulMurrayCbr.github.io
+git add Torchbearer
+git status
+git commit -m 'Torchbearer'
+git push
+popd
+
