780cab069a85724938b3bded3c8ccae6a9880593
Paul Murray <pmurray@bigpond.com>
2026-07-23 17:04:20 +1000

    PWA nonsense removed

--- publish.sh
+++ publish.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-cp -r ~/git/Torchbearer/web/* ~/git/PaulMurrayCbr.github.io/Torchbearer
+cp -r ~/git/Torchbearer/web/* ~/git/PaulMurrayCbr.github.io/Torchbearer0
 pushd ~/git/PaulMurrayCbr.github.io
-git add Torchbearer
+git add Torchbearer0
 git status
 git commit -m 'Torchbearer'
 git push
